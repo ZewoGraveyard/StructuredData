ad4f3a8eb2fbf504be51fbb7e8bd00b4dcf0bfa7
Paulo Faria <paulo.faria.rl@gmail.com>
2016-02-15 22:06:41 -0200

    it starts

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "InterchangeData",
+    dependencies: [
+        .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 2)
+    ]
+)
