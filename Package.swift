938c0c17a17fb02d65f4bdc2faca1a096a171633
Dylan Gyesbreghs <dylan.gyesbreghs@icapps.com>
2016-04-12 14:13:52 +0200

    Update package script to use new release version of Data

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "StructuredData",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 5),
     ]
 )
